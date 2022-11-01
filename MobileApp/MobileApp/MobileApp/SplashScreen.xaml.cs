fc14d9343f9cc1959f02f0980655a4898c9ee758
Yewo-Devs <85104248+Yewo-Devs@users.noreply.github.com>
2022-11-01 03:33:35 +0200

    change

---
+++ MobileApp/MobileApp/MobileApp/SplashScreen.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SplashScreen : ContentPage
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+            Splash();
+        }
+        public async void Splash()
+        {
+            P1.Opacity = 0;
+            P2.Opacity = 0;
+            await P1.FadeTo(1, 2000);
+            await P2.FadeTo(1, 2000);
+
+            await Navigation.PushAsync(new WebViewerPage());
+        }
+    }
+}
---
+++ MobileApp/MobileApp/MobileApp/WebViewerPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WebViewerPage : ContentPage
+    {
+        string baseLink = "";
+        string currentLink = "";
+        public WebViewerPage()
+        {
+            NavigationPage.SetHasNavigationBar(this, false);
+
+            InitializeComponent();
+
+            baseLink = "https://learning-system-hack.herokuapp.com/";
+
+            ShowWebView();
+        }
+
+        void ShowWebView()
+        {
+            view.Source = baseLink;
+            view.Navigating += View_Navigating;
+        }
+
+        private void View_Navigating(object sender, WebNavigatingEventArgs e)
+        {
+            currentLink = e.Url;
+        }
+
+        void GoBack()
+        {
+            view.GoBack();
+        }
+
+        public bool HandleBackButton()
+        {
+            GoBack();
+
+            return true;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            GoBack();
+        }
+    }
+}
